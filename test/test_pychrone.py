3e1257483b13422e3d0e8f8d827bc139e675b53b
Nikita <behind@me.com>
2018-07-02 19:35:17 +0300

    travis

--- test/test_pychrone.py
+++ test/test_pychrone.py
@@ -1,6 +1,10 @@
 import pytest
 import pychrone
+import geojson
 
 
-def test_negative():
+def test_none():
     assert (pychrone.Create_isochrone(37.847591, 55.920284, 5) !=None)
+
+def test_geojson():
+    assert (isinstance(pychrone.Create_isochrone(37.847591, 55.920284, 5), geojson.geometry.Polygon))
