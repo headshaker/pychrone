ffb3e72c5cd888719ac5011dfc918afc53c14476
Nikita <Nikita@MacBook-Pro-N.local>
2018-10-30 20:59:22 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
     name="pychrone",
-    version="0.0.2",
+    version="0.0.3",
     author="Nikita Chistikov",
     author_email="chistikov@gmail.com",
     description="Small python module for building isochrones",
